61663e5ff191a982ecc3124246200c4111a49d99
16023054 <16023054@myrp.edu.sg>
2018-04-23 13:00:52 +0800

    jjj

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,11 +1,15 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -17,13 +21,31 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        
-        al = new ArrayList<String>();
-        al.add("Year 1");
+        lv = (ListView) this.findViewById(R.id.lvYear);
 
 
+        List<String> years = new ArrayList<>();
+        years.add("Year 1");
+        years.add("Year 2");
+        years.add("Year 3");
 
-
+        aa = new ArrayAdapter(this, android.R.layout.simple_list_item_1 , years);
+        lv.setAdapter(aa);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent j = new Intent(FirstActivity.this, SecondActivity.class);
+                int pos = lv.getPositionForView(view);
+                if (pos==0){
+                    j.putExtra("year",0);
+                }
+                else if (pos==1){
+                    j.putExtra("year",1);
+                }else{
+                    j.putExtra("year",2);
+                }
+                startActivity(j);
+            }
+        });
     }
 }
